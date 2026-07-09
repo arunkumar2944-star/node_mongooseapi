5a0893951dce0005dd20def236c732b0e7ebba12
arunkumar <arunkumar_19@outlook.com>
2026-07-09 13:32:49 +0530

    user service completed and  implemented in angular

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  userName:{type:String,required:true},  
+  userName:{type:String,required:false},  
   password: { type: String, required: true },
   email: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
